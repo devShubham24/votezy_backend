a44ee520e4970c2eb4e93688d97208644af8c50f
Shubham <you@example.com>
2026-01-04 19:23:17 +0530

    minor change

--- src/main/java/com/vote/controller/CandidateController.java
+++ src/main/java/com/vote/controller/CandidateController.java
@@ -23,7 +23,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-@CrossOrigin
+@CrossOrigin(origins="*")
 @RestController
 @RequestMapping("/api")
 public class CandidateController {
